c412c1ebc3a7719141f18c4f647d31345a8bff78
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-10-01 14:41:01 -0700

    Create RecordFormatException.java

---
+++ Exceptions/ConvertDNA/RecordFormatException.java
@@ -0,0 +1,12 @@
+/**
+ * Allows the constructor for FastqRecord to throw an exception 
+ * by overriding the Exception class. 
+ * @author Uyen Nguyen
+ */
+
+public class RecordFormatException extends Exception
+{
+	public RecordFormatException(String message) {
+		super(message);
+	}
+}
