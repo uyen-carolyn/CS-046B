8f278f0fe0a057f50d080f157a6eb0017252475d
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-10-01 14:39:23 -0700

    Create DNARecord.java

---
+++ Exceptions/ConvertDNA/DNARecord.java
@@ -0,0 +1,10 @@
+/**
+ * FastqRecord and FastaRecord implements this. - (Homework Assignment Instructions) 
+ * @author Uyen Nguyen
+ */
+
+public interface DNARecord 
+{
+	String		getDefline();
+	String		getSequence();
+}
